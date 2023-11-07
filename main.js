826cd04982dc290df02dc47ceb787533e5671f8a
devantonio27 <antoniodev2709@gmail.com>
2023-11-07 15:38:47 -0300

    adicionando seleção de cards com utilizando array

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
+const ingressos = [] ;
+
 function highlightCard(selector){
     const element = document.querySelector(selector);
     element.classList.toggle("card-highlight");
 }
+
+function selectCard(selector){
+    const element = document.querySelector(selector);
+    element.classList.toggle("card-selected");
+    if(ingressos.includes(selector)) ingressos.pop(selector);
+    else ingressos.push(selector)
+}
+
+function showSelectedCards(){
+    if(ingressos.length > 0) alert("Ingressos Selecionados:" + ingressos);
+}
+
+addKeyboardEventListeners();
